6f0c8705665270a84845deae81de88498ef7bb6a
Craig Baylis <craig.baylis@gmail.com>
2017-03-14 20:34:05 -0400

    initial commit

---
+++ advanced_dice.rb
@@ -0,0 +1,14 @@
+continue = "y"
+puts "How many sides does your dice have?"
+sides = gets.chomp.to_i
+puts "How many times would you like to roll your pair of dice?"
+rolls = gets.chomp.to_i
+while continue.downcase != "n"
+rolls.times do
+dice1 = rand(sides) +1
+dice2 = rand(sides) +1
+puts "You rolled a #{dice1} and a #{dice2}. Total: #{dice1 + dice2}"
+end
+print "Roll again? (Y/n)> "
+continue = gets.chomp
+end
---
+++ dice.rb
@@ -0,0 +1,11 @@
+
+continue = nil
+
+while continue != "s"
+dice1 = rand(6) +1
+dice2 = rand(6) +1
+puts "You rolled a #{dice1} and a #{dice2}"
+puts "Total: #{dice1 + dice2}"
+puts "Press 'Enter' to roll again, 'S' to stop"
+continue = gets.chomp.downcase
+end
